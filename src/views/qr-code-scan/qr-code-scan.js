f689b62f1e27a6cfa2d98a622c381d3c40074627
sjhong98 <sjhong98@icloud.com>
2023-10-31 15:19:04 +0900

    feat: socket.io 연결

--- src/views/qr-code-scan/qr-code-scan.js
+++ src/views/qr-code-scan/qr-code-scan.js
@@ -1,8 +1,24 @@
 // import React, { useState, useEffect} from 'react';
 import Header from '../../modules/header.js';
 import Footer from '../../modules/footer.js';
+import { useEffect } from 'react';
+import io from 'socket.io-client';
 
 export default function QrCodeScan() {
+    const socket = io.connect('http://localhost:3001', {
+        cors: { origin: '*' },
+        reconnection: true, // 자동 재연결 활성화
+        reconnectionAttempts: 10, // 최대 재연결 시도 횟수
+        reconnectionDelay: 500, // 재연결 시도 간격
+      });
+
+    useEffect(() => {
+        const data = {
+            email: "test", 
+          }
+        socket.emit('login', data);
+    }, [])
+
     return(
         <div className='root'>
             <Header />
