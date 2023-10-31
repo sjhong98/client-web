24e4373a22118bcc477de638dec5e95468bc3f24
sjhong98 <sjhong98@icloud.com>
2023-10-31 15:41:41 +0900

    feat: app to web 통신 연결

--- src/views/qr-code-scan/qr-code-scan.js
+++ src/views/qr-code-scan/qr-code-scan.js
@@ -1,10 +1,10 @@
-// import React, { useState, useEffect} from 'react';
 import Header from '../../modules/header.js';
 import Footer from '../../modules/footer.js';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import io from 'socket.io-client';
 
 export default function QrCodeScan() {
+    const [jwt, setJwt] = useState("");
     const socket = io.connect('http://localhost:3001', {
         cors: { origin: '*' },
         reconnection: true, // 자동 재연결 활성화
@@ -13,11 +13,12 @@ export default function QrCodeScan() {
       });
 
     useEffect(() => {
-        const data = {
-            email: "test", 
-          }
-        socket.emit('login', data);
+        socket.emit('login', {email: "test", media: "web"});
     }, [])
+
+    socket.on('qr-stow', (data) => {
+        setJwt(data);
+    })
 
     return(
         <div className='root'>
@@ -25,7 +26,7 @@ export default function QrCodeScan() {
             <div className='body'>
                 <h1>QR 스캔</h1>
                 <p>모바일로 환자의 QR코드를 스캔해주세요.</p>
-                
+                <p>{jwt}</p>
             </div>
             <Footer />
         </div>
