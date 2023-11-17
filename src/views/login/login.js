cb6bb706f3699a98291f24ab98859daeb96a0841
sjhong98 <sjhong98@icloud.com>
2023-11-17 16:40:32 +0900

    feat: redirect url 변경

--- src/views/login/login.js
+++ src/views/login/login.js
@@ -5,8 +5,8 @@ export default function Login() {
 
     const Rest_api_key = process.env.REACT_APP_KAKAO_LOGIN; //REST API KEY
     const serverIP = process.env.REACT_APP_SERVER_IP_ADDRESS;
-    // const redirect_uri = `https://dmrs-b910d.web.app/login/auth` //Redirect URI
-    const redirect_uri = `http://localhost:3000/login/auth`
+    const redirect_uri = `https://dmrs-b910d.web.app/login/auth` //Redirect URI
+    // const redirect_uri = `http://localhost:3000/login/auth`
 
     console.log("here", Rest_api_key);
     console.log(serverIP)
