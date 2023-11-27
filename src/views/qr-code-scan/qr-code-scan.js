e3980ef7ab49318f6600a63c7c19404e4b19bb16
sjhong98 <sjhong98@icloud.com>
2023-11-27 23:09:54 +0900

    debug: eslint 수정

--- src/views/qr-code-scan/qr-code-scan.js
+++ src/views/qr-code-scan/qr-code-scan.js
@@ -9,7 +9,6 @@ import { useState } from 'react';
 export default function QrCodeScan() {
     const navigate = useNavigate();
     const dispatch = useDispatch();
-    const testJwt = {email:"test", name:"홍승재", jwt:"eyJhbGciOiJFUzI1NkstUiIsInR5cCI6IkpXVCJ9.eyJ2YyI6eyJAY29udGV4dCI6WyJodHRwczovL3d3dy53My5vcmcvMjAxOC9jcmVkZW50aWFscy92MSJdLCJ0eXBlIjpbIlZlcmlmaWFibGVDcmVkZW50aWFsIl0sImNyZWRlbnRpYWxTdWJqZWN0Ijp7Imlzc3VlciI6eyJuYW1lIjoiTWVkaWNhbCBSZWNvcmQgTWFuYWdlbWVudCBBc3NvY2lhdGlvbiIsImFkZHJlc3MiOiIweDNGZTdEQjQ3MDcyMDBlY0RlN2Q0Nzg4YjgwNWYyMjU2RTNiQzQ4NjcifSwiaG9zcGl0YWwiOiLshJzsmrjrs5Hsm5AiLCJtZWRpY2FsUmVjb3JkcyI6W119fSwic3ViIjoie1wiZGlkXCI6XCJkaWQ6ZXRocjpnb2VybGk6MHgyQ0IxNzVBOTcyMDMwNjQzQjhkMmYxNjlFMzUxZTM5MzcwMmE4ODZhXCIsXCJhZGRyZXNzXCI6XCIweDJDQjE3NUE5NzIwMzA2NDNCOGQyZjE2OUUzNTFlMzkzNzAyYTg4NmFcIn0iLCJpc3MiOiJkaWQ6ZXRocjpnb2VybGk6MHg1ZEQ3MDU2MUQ0MjRFNDQxYzYwODIyMDJENkU2NDE2RjdmZkMwMTA5In0._4F0xGeyckEI_sG1uDhL3n2kekpxnQgCTN7lf2jAtQQsYZGbc5rVjtySV4IUubVvv7Qbr9GgL5Lt4Njk3cS75wA"}
     const [patientJwt, _setPatientJwt] = useState("");
     const [vpJwt, setVpJwt] = useState("");
     const [link, setLink] = useState("");
@@ -34,7 +33,7 @@ export default function QrCodeScan() {
                     did: did
                 })
                 .then(res => {
-                    console.log("", res);    
+                    console.log("===== record/vp =====\n", res);    
                     dispatch(setJwtObj(res.data.medicalRecords));
                     dispatch(setPatientJwt(patientJwt));
                     dispatch(setPatientName("TEST"));
